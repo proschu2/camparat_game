968891f18fb3f1d4fd5b2ca919f6ba4f0ea9a412
proschu2 <github@sanziomonti.xyz>
2024-08-18 23:27:58 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+import 'package:camparat_game/camparat_game.dart';
+
+void main() {
+  runApp(
+    GameWidget(game: TRexGame()),
+  );
+}
